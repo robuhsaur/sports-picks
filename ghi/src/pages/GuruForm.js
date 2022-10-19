dc62f2145a010e83ac168875fad3f03a78e62225
Robert Nacario <r.nacariojr24@gmail.com>
2022-10-19 13:06:43 -0700

    able to get guru ID in getGuruId function

--- ghi/src/pages/GuruForm.js
+++ ghi/src/pages/GuruForm.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useState } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useAuthContext } from '../Auth'
+
 
 
 function BootStrapInput(props) {
@@ -15,34 +17,80 @@ function BootStrapInput(props) {
     )
 }
 
-function GuruForm(props) {
+// if form is there for specific guru (token?)
+// make a get request from /guru/{guru_id}/form
+// if response is null, post
+// else post form to /gurus/form 
 
+
+function GuruForm(props) {
+    const { token } = useAuthContext()
+    console.log(token)
     const [pick, setPick] = useState('')
     const [pickDetail, setPickDetail] = useState('')
+    const navigate = useNavigate()
+    const [guruId, setGuruId] = useState()
 
-    async function handleSubmit(e) {
+    async function getGuruId(e) {
         e.preventDefault();
-        const url = `http://localhost:8000/gurus/form`
-        const response = await fetch(url, {
-            method: "post",
+
+        const guruIdUrl = `http://localhost:8000/guruinfo`
+        const response = await fetch(guruIdUrl, {
+            method: "get",
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
             },
-            body: JSON.stringify({ pick, pickDetail })
         })
-
         const data = await response.json()
+        const guruId = data["id"] // guru id
+        setGuruId(guruId)
+        console.log(guruId)
+    }
 
+    async function getGuruForms(e) {
+        e.preventDefault();
+        const guru_id = guruId
+        const guruForms = `http://localhost:8000/guru/${guru_id}/form`
+        const response = await fetch(guruForms, {
+            method: "get",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        const data = await response.json()
+        console.log(data)
         if (response.ok) {
-            Navigate("/guru/signup")
-        } else {
-            console.log("didn't work uh oh")
+            console.log("hello it work")
         }
+
     }
 
 
 
 
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const pick_detail = pickDetail
+        const url = `http://localhost:8000/gurus/form`
+        const response = await fetch(url, {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({ pick, pick_detail })
+        })
+        const data = await response.json()
+
+        if (response.ok) {
+            console.log("not stinky")
+            navigate("/guru/signup")
+        } else {
+            console.log("uh oh stinky")
+        }
+    }
 
     return (
         <form>
@@ -62,10 +110,11 @@ function GuruForm(props) {
                 type="text" />
 
             <button onClick={handleSubmit}> Submit </button>
+            <button onClick={getGuruId}> ID </button>
+            <button onClick={getGuruForms}> Forms </button>
         </form>
     )
 }
-
 export default GuruForm
 
 
--- ghi/src/pages/Home.js
+++ ghi/src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     return (<Grid container justifyContent='center' alignItems='center' >
         <Box textAlign={"center"}>
             <Button variant="contained" onClick={() => navigate("/signup-guru")}>Sign Up As Guru</Button>
-            <Button variant="contained" onClick={() => navigate("/signup-user")}>Login As Guru</Button>
+            <Button variant="contained" onClick={() => navigate("/login-guru")}>Login As Guru</Button>
             <Button variant="contained" onClick={() => navigate("/signup-user")}>Sign Up As User</Button>
             <Button variant="contained" onClick={() => navigate("/login-user")}>Log In As User</Button>
         </Box>
