56c9a1d66ba8e6f3b57bba04fab1e16003671ebc
Robert Nacario <r.nacariojr24@gmail.com>
2022-10-24 16:05:37 -0700

    fixed nav bar

--- ghi/src/pages/Nav.js
+++ ghi/src/pages/Nav.js
@@ -12,13 +12,12 @@ function Nav() {
                 <div className="container-fluid">
                     <NavLink className="text-decoration-underline navbar-brand" to="/">OnlyPics</NavLink>
                     <ul className="dropdown">
-                        <NavLink className="btn btn-dark" to="/guru/create" type="button" aria-current="page"> Guru </NavLink>
+                        <NavLink className="btn btn-dark" to="/guru/create" type="button" aria-current="page"> Make a Guru Form </NavLink>
                         <NavLink className="btn btn-dark" to="/sportslist" type="button" aria-current="page"> Sports </NavLink>
                         <NavLink className="btn btn-dark" to="/" type="button" aria-current="page"> My Gurus </NavLink>
-                        <NavLink className="btn btn-dark" to="/gurus" type="button" aria-current="page">  Gurus </NavLink>
 
                         {
-                            token && <button type="button" className="btn btn-dark" onClick={()=> navigate('/gurus')} >Gurus</button>
+                            token && <button type="button" className="btn btn-dark" onClick={() => navigate('/gurus')} >Gurus</button>
                         }
                         {
                             token && <button type="submit" className="btn btn-danger" onClick={logout}> Logout </button>
