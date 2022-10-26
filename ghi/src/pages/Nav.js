e279f280d63691fff23be974633ec560ba555dd4
Armen Ohanian <nassirarmen@gmail.com>
2022-10-26 18:34:56 +0000

    Armen

--- ghi/src/pages/Nav.js
+++ ghi/src/pages/Nav.js
@@ -1,34 +1,68 @@
-import { NavLink } from 'react-router-dom';
-import { useToken } from '../Auth'
-import { useNavigate } from 'react-router-dom';
-
+import { NavLink } from "react-router-dom";
+import { useToken } from "../Auth";
+import { useNavigate } from "react-router-dom";
 
 function Nav() {
-    const token = useToken();
-    const tokenNav = token[0]
-    const logout = token[2]
-    const navigate = useNavigate();
-    return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container-fluid">
-                    <NavLink className="text-decoration-underline navbar-brand" to="/">OnlyPicks</NavLink>
-                    <ul className="dropdown">
-                        <NavLink className="btn btn-dark" to="/guru/create" type="button" aria-current="page"> Make a Guru Form </NavLink>
-                        <NavLink className="btn btn-dark" to="/sportslist" type="button" aria-current="page"> Sports </NavLink>
-                        <NavLink className="btn btn-dark" to="/" type="button" aria-current="page"> My Gurus </NavLink>
+  const token = useToken();
+  const tokenNav = token[0];
+  const logout = token[2];
+  const navigate = useNavigate();
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <NavLink className="text-decoration-underline navbar-brand" to="/">
+            OnlyPicks
+          </NavLink>
+          <ul className="dropdown">
+            <NavLink
+              className="btn btn-dark"
+              to="/guru/create"
+              type="button"
+              aria-current="page"
+            >
+              {" "}
+              Make a Guru Form{" "}
+            </NavLink>
+            <NavLink
+              className="btn btn-dark"
+              to="/sportslist"
+              type="button"
+              aria-current="page"
+            >
+              {" "}
+              Sports{" "}
+            </NavLink>
+            <NavLink
+              className="btn btn-dark"
+              to="/my-gurus"
+              type="button"
+              aria-current="page"
+            >
+              {" "}
+              My Gurus{" "}
+            </NavLink>
 
-                        {
-                            tokenNav && <button type="button" className="btn btn-dark" onClick={() => navigate('/gurus')} >Gurus</button>
-                        }
-                        {
-                            tokenNav && <button type="submit" className="btn btn-danger" onClick={logout}> Logout </button>
-                        }
-                    </ul>
-                </div>
-            </nav>
-        </>
-    )
-};
+            {tokenNav && (
+              <button
+                type="button"
+                className="btn btn-dark"
+                onClick={() => navigate("/gurus")}
+              >
+                Gurus
+              </button>
+            )}
+            {tokenNav && (
+              <button type="submit" className="btn btn-danger" onClick={logout}>
+                {" "}
+                Logout{" "}
+              </button>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
 
 export default Nav;
